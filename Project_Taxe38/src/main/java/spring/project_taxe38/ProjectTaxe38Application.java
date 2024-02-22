e95c6689c186ea12606c631af3c532f8e156b6cd
abdelilah erraguibi <erraguibiabdelilah1@gmail.com>
2024-02-22 13:57:34 +0100

    first commit

---
+++ Project_Taxe38/src/main/java/spring/project_taxe38/ProjectTaxe38Application.java
@@ -0,0 +1,13 @@
+package spring.project_taxe38;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTaxe38Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectTaxe38Application.class, args);
+    }
+
+}
---
+++ Project_Taxe38/src/test/java/spring/project_taxe38/ProjectTaxe38ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.project_taxe38;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTaxe38ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
