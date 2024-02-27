30862a5a514d2364240234ac78721845bdb69092
Mohammed Ezzaim <m.ezzaim2139@uca.ac>
2024-02-27 11:46:30 +0100

    update mohammed ezzaim

--- project-spring-sir/src/main/java/usa/harvard/projectspringsir/dao/Locale38DetailDao.java
+++ project-spring-sir/src/main/java/usa/harvard/projectspringsir/dao/Locale38DetailDao.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import usa.harvard.projectspringsir.bean.Locale;
 import usa.harvard.projectspringsir.bean.Locale38Detail;
-
 import java.util.Optional;
 @Repository
 public interface Locale38DetailDao extends JpaRepository<Locale38Detail,Long> {
