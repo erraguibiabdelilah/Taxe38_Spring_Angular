6e7ea8427761ac8b1d4110812f35c50df0181aa2
Mohammed Ezzaim <m.ezzaim2139@uca.ac>
2024-03-10 12:01:05 +0000

    Committing local changes to VilleWs.java before pulling

--- project-spring-sir/src/main/java/usa/harvard/projectspringsir/ws/facade/VilleWs.java
+++ project-spring-sir/src/main/java/usa/harvard/projectspringsir/ws/facade/VilleWs.java
@@ -17,23 +17,19 @@ public class VilleWs {
     public VilleWs(VilleServiceImpl villeService) {
         this.villeService = villeService;
     }
-
     @DeleteMapping("/code/{code}")
     @Transactional
     public int deleteByCode(@PathVariable String code) {
         return villeService.deleteByCode(code);
     }
-
     @GetMapping("/code/{code}")
     public Ville findByCode(@PathVariable String code) {
         return villeService.findByCode(code);
     }
-
     @PostMapping("/")
     public int save(@RequestBody Ville ville) {
         return villeService.save(ville);
     }
-
     @GetMapping("/")
     public List<Ville> findAll() {
         return villeService.findAll();
