6ee2921f5cbdb013be09a7d84d37e4afc0448c16
Mohammed Ezzaim <m.ezzaim2139@uca.ac>
2024-03-10 09:52:15 +0000

    succes!

--- project-spring-sir/src/main/java/usa/harvard/projectspringsir/ws/facade/VilleWs.java
+++ project-spring-sir/src/main/java/usa/harvard/projectspringsir/ws/facade/VilleWs.java
@@ -3,14 +3,16 @@ package usa.harvard.projectspringsir.ws.facade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-import usa.harvard.projectspringsir.bean.commun.Ville;
-import usa.harvard.projectspringsir.service.imple.VilleServiceImpl;
+import usa.harvard.projectspringsir.bean.communBean.Ville;
+import usa.harvard.projectspringsir.service.imple.communImpl.VilleServiceImpl;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("api/ville")
+@CrossOrigin("*")
 public class VilleWs {
+    @Autowired
     private  VilleServiceImpl villeService;
     public VilleWs(VilleServiceImpl villeService) {
         this.villeService = villeService;
